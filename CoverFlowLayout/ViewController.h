0b521c484d48795add4c4c5eb7f4df45f1eb9c10
neliSun <nelson.chow91.nc@gmail.com>
2016-03-24 11:28:24 -0700

    Initial Commit

---
+++ CoverFlowLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoverFlowLayout
+//
+//  Created by Nelson Chow on 2016-03-24.
+//  Copyright © 2016 Nelson Chow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
